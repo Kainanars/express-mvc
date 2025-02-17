aa2802b1e9fea9988fb5cb675054206de655e5c7
Kainan Rodrigues <kainan.ars@gmail.com>
2025-02-17 09:06:59 -0300

    feat: adicionar arquivo .gitignore, remover produtos e adicionar perfil

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var productsRouter = require('./routes/products');
-var aboutRouter = require('./routes/about');
-var helloRouter = require('./routes/hello');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,11 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
-app.use('/description', productsRouter);
-app.use('/about', aboutRouter);
-app.use('/hello', helloRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));

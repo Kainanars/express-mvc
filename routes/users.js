f21ea54739930a9175573fe6788a90d5ace02204
Kainan Rodrigues <kainan.ars@gmail.com>
2025-02-17 09:06:43 -0300

    feat: remover rotas obsoletas e atualizar a rota principal para usar perfil

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function (req, res, next) {
-  res.send('Kainan Rodrigues'.bold().fontsize(50));
-});
-
-module.exports = router;
